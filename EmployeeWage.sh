b94d3d948c9a631d3e8f39b536b966ea41c8d0dc
dbhide <dhanashree.bhide3@gmail.com>
2020-01-23 12:54:10 +0530

    MODIFIED: Program re-written using Case statements

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -5,15 +5,17 @@ echo "Welcome to Employee Wage Computation Program"
 PART_TIME_EMPLOYEE=1;
 FULL_TIME_EMPLOYEE=2;
 EMP_RATE_PER_HR=20;
-EMPLOYEE_CHECK=$((RANDOM%2));
+EMPLOYEE_CHECK=$((RANDOM%3));
 
-if [ $FULL_TIME_EMPLOYEE -eq $EMPLOYEE_CHECK ]
-then
-	EmpHours=8
-elif [ $PART_TIME_EMPLOYEE -eq $EMPLOYEE_CHECK ]
-then
-	EmpHours=4
-else
-	EmpHours=0
-fi
-salary=$(($EmpHours*$EMP_RATE_PER_HR))
+case $EMPLOYEE_CHECK in
+			$FULL_TIME_EMPLOYEE)
+				empHrs=8;;
+
+			$PART_TIME_EMPLOYEE)
+				empHrs=4;;
+
+			*)
+			empHrs=0;;
+esac
+
+salary=$(($empHrs*$EMP_RATE_PER_HR));
